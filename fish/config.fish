372f7aae888bdd2e90e3ff5eed6f75bf1a0cbe47
reujab <reujab@gmail.com>
2017-07-10 02:19:31 -0400

    disabled fish greeting

--- fish/config.fish
+++ fish/config.fish
@@ -2,5 +2,6 @@ set -gx PATH $PATH ~/go/bin
 set -x SHELL fish
 set -x EDITOR nvim
 set -x LESSHISTFILE ~/.cache/.lesshist
+set fish_greeting
 set BRONZE black:white:status blue:black:shortdir green:black:git magenta:black:cmdtime
 eval (bronze init)
