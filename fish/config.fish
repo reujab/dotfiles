bc488cee633a5ff7def7afb1d70eaf8a28d867a4
reujab <reujab@gmail.com>
2017-07-19 05:54:26 -0400

    pacaur -> packages

--- fish/config.fish
+++ fish/config.fish
@@ -3,6 +3,6 @@ set -x SHELL fish
 set -x EDITOR nvim
 set -x LESSHISTFILE ~/.cache/.lesshist
 set fish_greeting
-set BRONZE status:black:white pacaur:black:white dir:blue:black git:green:black cmdtime:magenta:black
+set BRONZE status:black:white packages:black:white dir:blue:black git:green:black cmdtime:magenta:black
 set -x BRONZE_DIR_LENGTH 1
 eval (bronze init)
