6e543c801048c6d201c683ffbf5987831c25e25b
reujab <reujab@gmail.com>
2017-07-30 23:23:41 -0400

    added rss module to bronze

--- fish/config.fish
+++ fish/config.fish
@@ -1,10 +1,16 @@
+# disable fish greeting
+set fish_greeting
+
 set -gx PATH $PATH ~/go/bin
-set -x SHELL fish
+
 set -x EDITOR nvim
+
+# less
 set -x LESS -R
 set -x LESSHISTFILE ~/.cache/.lesshist
-set fish_greeting
-set BRONZE status:black:white packages:black:white dir:blue:black git:green:black cmdtime:magenta:black
+
+# bronze
+set BRONZE status:black:white packages:black:white rss:black:white dir:blue:black git:green:black cmdtime:magenta:black
 set -x BRONZE_SHELL fish
 set -x BRONZE_DIR_ALIASES ~/go/src/github.com/reujab:\ue627
 set -x BRONZE_DIR_LENGTH 1
